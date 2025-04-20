00f3c329e6cc6d0dca7ce8e13e07196dc10ccc48
Talal Zafar <talalzafar84@gmail.com>
2025-04-20 12:20:46 +0500

    Add langchain-core to requirements

--- app/model/llama3_1_8b_instant.py
+++ app/model/llama3_1_8b_instant.py
@@ -8,6 +8,8 @@ load_dotenv()
 def InitializeModel():
     GROQ_API_KEY1=os.getenv("GROQ_API_KEY_T")
     GROQ_API_KEY2=os.getenv("GROQ_API_KEY_R")
+    GROQ_API_KEY2=os.getenv("GROQ_API_KEY_C_T")
     chat1 = ChatGroq(temperature=0.1, groq_api_key=GROQ_API_KEY1, model_name="llama-3.1-8b-instant")
     chat2 = ChatGroq(temperature=0.1, groq_api_key=GROQ_API_KEY2, model_name="llama-3.1-8b-instant")
-    return chat1,chat2
+    chat3 = ChatGroq(temperature=0.1, groq_api_key=GROQ_API_KEY2, model_name="llama-3.1-8b-instant")
+    return chat1,chat2,chat3
